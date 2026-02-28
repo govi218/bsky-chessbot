f97625cc98962778b0331409df011f7587a692c1
Jost Triller <tsoj.tsoj@gmx.de>
2026-02-28 03:13:11 +0100

    Stuff

--- download_website_screenshots.sh
+++ download_website_screenshots.sh
@@ -5,7 +5,7 @@ set -e
 # I found this here https://www.kaggle.com/datasets/aydosphd/webscreenshots
 mkdir -p resources/website_screenshots
 cd resources/website_screenshots
-gdown --folder https://drive.google.com/drive/folders/1cNjcwM0rc8Wr9dESrsxytKwXl9j3QnbR
+uv run gdown --folder https://drive.google.com/drive/folders/1cNjcwM0rc8Wr9dESrsxytKwXl9j3QnbR
 7za x screenshots-1440x900/machinery.7z
 7za x screenshots-1440x900/music.7z
 7za x screenshots-1440x900/sport.7z
