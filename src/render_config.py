d96c7387f870e5cfd243f5c49024589f088e5275
Jost Triller <tsoj.tsoj@gmx.de>
2026-03-04 00:39:43 +0100

    Stuff

--- src/render_config.py
+++ src/render_config.py
@@ -7,6 +7,9 @@ from PIL import Image
 
 from src.games import GameSpec, get_game
 
+PIECES_DIR = Path("resources/pieces")
+PIECE_EXTENSIONS = {".svg", ".png"}
+
 
 @dataclass(frozen=True)
 class PieceSetConfig:
@@ -21,260 +24,32 @@ class GameRenderConfig:
     piece_file_names_by_provider: dict[str, tuple[str, ...]]
 
 
-CHESS_RENDER_CONFIG = GameRenderConfig(
-    piece_sets=(
-        PieceSetConfig("lichess", "alpha"),
-        PieceSetConfig("lichess", "caliente"),
-        PieceSetConfig("lichess", "california"),
-        PieceSetConfig("lichess", "cardinal"),
-        PieceSetConfig("lichess", "cburnett"),
-        PieceSetConfig("lichess", "celtic"),
-        PieceSetConfig("lichess", "chess7"),
-        PieceSetConfig("lichess", "chessnut"),
-        PieceSetConfig("lichess", "companion"),
-        PieceSetConfig("lichess", "dubrovny"),
-        PieceSetConfig("lichess", "fantasy"),
-        PieceSetConfig("lichess", "fresca"),
-        PieceSetConfig("lichess", "gioco"),
-        PieceSetConfig("lichess", "governor"),
-        PieceSetConfig("lichess", "icpieces"),
-        PieceSetConfig("lichess", "kiwen-suwi"),
-        PieceSetConfig("lichess", "kosal"),
-        PieceSetConfig("lichess", "leipzig"),
-        PieceSetConfig("lichess", "libra"),
-        PieceSetConfig("lichess", "maestro"),
-        PieceSetConfig("lichess", "merida"),
-        PieceSetConfig("lichess", "mpchess"),
-        PieceSetConfig("lichess", "pirouetti"),
-        PieceSetConfig("lichess", "pixel"),
-        PieceSetConfig("lichess", "reillycraig"),
-        PieceSetConfig("lichess", "riohacha"),
-        PieceSetConfig("lichess", "spatial"),
-        PieceSetConfig("lichess", "staunty"),
-        PieceSetConfig("lichess", "tatiana"),
-        PieceSetConfig("extra", "8_bit"),
-        PieceSetConfig("extra", "bases"),
-        PieceSetConfig("extra", "book"),
-        PieceSetConfig("extra", "bubblegum"),
-        PieceSetConfig("extra", "cases"),
-        PieceSetConfig("extra", "celtic"),
-        PieceSetConfig("extra", "chicago"),
-        PieceSetConfig("extra", "classic"),
-        PieceSetConfig("extra", "club"),
-        PieceSetConfig("extra", "condal"),
-        PieceSetConfig("extra", "dash"),
-        PieceSetConfig("extra", "eyes"),
-        PieceSetConfig("extra", "falcon"),
-        PieceSetConfig("extra", "fantasy_alt"),
-        PieceSetConfig("extra", "game_room"),
-        PieceSetConfig("extra", "glass"),
-        PieceSetConfig("extra", "gothic"),
-        PieceSetConfig("extra", "graffiti"),
-        PieceSetConfig("extra", "icy_sea"),
-        PieceSetConfig("extra", "iowa"),
-        PieceSetConfig("extra", "light"),
-        PieceSetConfig("extra", "lolz"),
-        PieceSetConfig("extra", "marble"),
-        PieceSetConfig("extra", "maya"),
-        PieceSetConfig("extra", "metal"),
-        PieceSetConfig("extra", "modern"),
-        PieceSetConfig("extra", "nature"),
-        PieceSetConfig("extra", "neo"),
-        PieceSetConfig("extra", "neo_wood"),
-        PieceSetConfig("extra", "neon"),
-        PieceSetConfig("extra", "newspaper"),
-        PieceSetConfig("extra", "ocean"),
-        PieceSetConfig("extra", "oslo"),
-        PieceSetConfig("extra", "royale"),
-        PieceSetConfig("extra", "sky"),
-        PieceSetConfig("extra", "space"),
-        PieceSetConfig("extra", "spatial"),
-        PieceSetConfig("extra", "tigers"),
-        PieceSetConfig("extra", "tournament"),
-        PieceSetConfig("extra", "vintage"),
-        PieceSetConfig("extra", "wood"),
-        PieceSetConfig("custom", "a"),
-        PieceSetConfig("custom", "b"),
-        PieceSetConfig("custom", "c"),
-        PieceSetConfig("custom", "d"),
-        PieceSetConfig("custom", "e"),
-        PieceSetConfig("pychess", "atopdown"),
-        PieceSetConfig("pychess", "luffy"),
-        PieceSetConfig("pychess", "santa"),
-        PieceSetConfig("other", "chess_1Kbyte_gambit"),
-        PieceSetConfig("other", "chess_kaneo"),
-        PieceSetConfig("other", "chess_kaneo_midnight"),
-        PieceSetConfig("other", "chess_maestro_bw"),
-    ),
-    piece_file_names_by_provider={
-        "lichess": ("bB.svg", "bK.svg", "bN.svg", "bP.svg", "bQ.svg", "bR.svg", "wB.svg", "wK.svg", "wN.svg", "wP.svg", "wQ.svg", "wR.svg"),
-        "extra": ("bb.png", "bk.png", "bn.png", "bp.png", "bq.png", "br.png", "wb.png", "wk.png", "wn.png", "wp.png", "wq.png", "wr.png"),
-        "custom": ("bb.png", "bk.png", "bn.png", "bp.png", "bq.png", "br.png", "wb.png", "wk.png", "wn.png", "wp.png", "wq.png", "wr.png"),
-        "pychess": ("bB.svg", "bK.svg", "bN.svg", "bP.svg", "bQ.svg", "bR.svg", "wB.svg", "wK.svg", "wN.svg", "wP.svg", "wQ.svg", "wR.svg",
-                    "bb.svg", "bk.svg", "bn.svg", "bp.svg", "bq.svg", "br.svg", "wb.svg", "wk.svg", "wn.svg", "wp.svg", "wq.svg", "wr.svg",
-                    "bB.png", "bK.png", "bN.png", "bP.png", "bQ.png", "bR.png", "wB.png", "wK.png", "wN.png", "wP.png", "wQ.png", "wR.png"),
-        "other": ("bB.svg", "bK.svg", "bN.svg", "bP.svg", "bQ.svg", "bR.svg", "wB.svg", "wK.svg", "wN.svg", "wP.svg", "wQ.svg", "wR.svg"),
-    },
-)
+def _discover_render_config(game: str) -> GameRenderConfig:
+    game_dir = PIECES_DIR / game
+    if not game_dir.is_dir():
+        return GameRenderConfig(piece_sets=(), piece_file_names_by_provider={})
 
+    piece_sets: list[PieceSetConfig] = []
+    file_names_by_provider: dict[str, set[str]] = {}
 
-XIANGQI_RENDER_CONFIG = GameRenderConfig(
-    piece_sets=(
-        PieceSetConfig("pychess", "2dhanzi"),
-        PieceSetConfig("pychess", "2dintl"),
-        PieceSetConfig("pychess", "ct2"),
-        PieceSetConfig("pychess", "ct2w"),
-        PieceSetConfig("pychess", "ct3"),
-        PieceSetConfig("pychess", "euro"),
-        PieceSetConfig("pychess", "eventhanzi"),
-        PieceSetConfig("pychess", "eventintl"),
-        PieceSetConfig("pychess", "hnz"),
-        PieceSetConfig("pychess", "hnzw"),
-        PieceSetConfig("pychess", "Ka"),
-        PieceSetConfig("pychess", "lishu"),
-        PieceSetConfig("pychess", "lishuw"),
-        PieceSetConfig("pychess", "playok"),
-        PieceSetConfig("pychess", "ttxqhanzi"),
-        PieceSetConfig("pychess", "ttxqintl"),
-        PieceSetConfig("pychess", "wikim"),
-        PieceSetConfig("other", "xiangqi_gmchess_alternative_black_red"),
-        PieceSetConfig("other", "xiangqi_gmchess_alternative_gray"),
-        PieceSetConfig("other", "xiangqi_gmchess_style_wood"),
-        PieceSetConfig("other", "xiangqi_wikipedia_intl_modded"),
-        PieceSetConfig("other", "ccbridge_3_0_beta4_default_preview_remake"),
-        PieceSetConfig("other", "commons_xiangqi_pieces_print_2010"),
-        PieceSetConfig("other", "commons_xiangqi_pieces_print_2010_bw_heavy"),
-        PieceSetConfig("other", "euro_xiangqi_js"),
-        PieceSetConfig("other", "euro_xiangqi_js_tricolor"),
-        PieceSetConfig("other", "latex_xqlarge_2006_chinese_autotrace"),
-        PieceSetConfig("other", "latex_xqlarge_2006_chinese_potrace"),
-        PieceSetConfig("other", "playok_2014_chinese"),
-        PieceSetConfig("other", "playok_2014_chinese_noshadow"),
-        PieceSetConfig("other", "retro_simple"),
-    ),
-    piece_file_names_by_provider={
-        "pychess": (
-            "wR.svg", "wN.svg", "wB.svg", "wA.svg", "wK.svg", "wC.svg", "wP.svg",
-            "bR.svg", "bN.svg", "bB.svg", "bA.svg", "bK.svg", "bC.svg", "bP.svg",
-        ),
-        "other": (
-            "wR.svg", "wN.svg", "wB.svg", "wA.svg", "wK.svg", "wC.svg", "wP.svg",
-            "bR.svg", "bN.svg", "bB.svg", "bA.svg", "bK.svg", "bC.svg", "bP.svg",
-        ),
-    },
-)
+    for provider_dir in sorted(game_dir.iterdir()):
+        if not provider_dir.is_dir():
+            continue
+        provider = provider_dir.name
+        for set_dir in sorted(provider_dir.iterdir()):
+            if not set_dir.is_dir():
+                continue
+            piece_sets.append(PieceSetConfig(provider, set_dir.name))
+            for f in set_dir.iterdir():
+                if f.suffix.lower() in PIECE_EXTENSIONS:
+                    file_names_by_provider.setdefault(provider, set()).add(f.name)
 
-
-SHOGI_RENDER_CONFIG = GameRenderConfig(
-    piece_sets=(
-        PieceSetConfig("pychess", "2kanji"),
-        PieceSetConfig("pychess", "bnw"),
-        PieceSetConfig("pychess", "bw"),
-        PieceSetConfig("pychess", "ctim"),
-        PieceSetConfig("pychess", "ctk"),
-        PieceSetConfig("pychess", "ctkw3d"),
-        PieceSetConfig("pychess", "ctm"),
-        PieceSetConfig("pychess", "ctp"),
-        PieceSetConfig("pychess", "ctp3d"),
-        PieceSetConfig("pychess", "ctw"),
-        PieceSetConfig("pychess", "cz"),
-        PieceSetConfig("pychess", "Ka"),
-        PieceSetConfig("pychess", "Portella-Intl"),
-        PieceSetConfig("pychess", "Portella-Kanji"),
-        PieceSetConfig("other", "2-kanji_red_wood"),
-        PieceSetConfig("other", "international"),
-        PieceSetConfig("other", "kanji_brown"),
-        PieceSetConfig("other", "kanji_light"),
-        PieceSetConfig("other", "kanji_light_3D_OTB"),
-        PieceSetConfig("other", "kanji_red_wood"),
-        PieceSetConfig("other", "military"),
-        PieceSetConfig("lishogi", "1kanji_3d"),
-        PieceSetConfig("lishogi", "2kanji_3d"),
-        PieceSetConfig("lishogi", "alfaerie"),
-        PieceSetConfig("lishogi", "better_8_bit"),
-        PieceSetConfig("lishogi", "characters"),
-        PieceSetConfig("lishogi", "dewitt_1kanji"),
-        PieceSetConfig("lishogi", "dewitt_2kanji"),
-        PieceSetConfig("lishogi", "dewitt_czech"),
-        PieceSetConfig("lishogi", "dobutsu"),
-        PieceSetConfig("lishogi", "engraved_cz"),
-        PieceSetConfig("lishogi", "engraved_cz_bnw"),
-        PieceSetConfig("lishogi", "firi"),
-        PieceSetConfig("lishogi", "glass"),
-        PieceSetConfig("lishogi", "greenwade"),
-        PieceSetConfig("lishogi", "hitomoji"),
-        PieceSetConfig("lishogi", "international"),
-        PieceSetConfig("lishogi", "intl_colored_2d"),
-        PieceSetConfig("lishogi", "intl_colored_3d"),
-        PieceSetConfig("lishogi", "intl_monochrome_2d"),
-        PieceSetConfig("lishogi", "intl_portella"),
-        PieceSetConfig("lishogi", "intl_shadowed"),
-        PieceSetConfig("lishogi", "intl_wooden_3d"),
-        PieceSetConfig("lishogi", "joyful"),
-        PieceSetConfig("lishogi", "kanji_brown"),
-        PieceSetConfig("lishogi", "kanji_guide_shadowed"),
-        PieceSetConfig("lishogi", "kanji_light"),
-        PieceSetConfig("lishogi", "kanji_red_wood"),
-        PieceSetConfig("lishogi", "logy_games"),
-        PieceSetConfig("lishogi", "mnemonic"),
-        PieceSetConfig("lishogi", "orangain"),
-        PieceSetConfig("lishogi", "pixel"),
-        PieceSetConfig("lishogi", "portella"),
-        PieceSetConfig("lishogi", "portella_2kanji"),
-        PieceSetConfig("lishogi", "ryoko_1kanji"),
-        PieceSetConfig("lishogi", "shogi_bnw"),
-        PieceSetConfig("lishogi", "shogi_cz"),
-        PieceSetConfig("lishogi", "shogi_fcz"),
-        PieceSetConfig("lishogi", "simple_kanji"),
-        PieceSetConfig("lishogi", "vald_opt"),
-        PieceSetConfig("lishogi", "valdivia"),
-        PieceSetConfig("lishogi", "western"),
-        PieceSetConfig("shogi-themes", "13xforever_1kanji"),
-        PieceSetConfig("shogi-themes", "13xforever_2kanji"),
-        PieceSetConfig("shogi-themes", "hari_seldon_1kanji"),
-        PieceSetConfig("shogi-themes", "hari_seldon_2kanji"),
-        PieceSetConfig("shogi-themes", "kinki_1kanji"),
-        PieceSetConfig("shogi-themes", "kinki_2kanji"),
-        PieceSetConfig("shogi-themes", "minase_1kanji"),
-        PieceSetConfig("shogi-themes", "minase_2kanji"),
-        PieceSetConfig("shogi-themes", "ryoko_1kanji"),
-        PieceSetConfig("shogi-themes", "ryoko_2kanji"),
-    ),
-    piece_file_names_by_provider={
-        "pychess": (
-            "wK.svg", "wR.svg", "wB.svg", "wG.svg", "wS.svg", "wN.svg", "wL.svg", "wP.svg",
-            "w+R.svg", "w+B.svg", "w+S.svg", "w+N.svg", "w+L.svg", "w+P.svg",
-            "bK.svg", "bR.svg", "bB.svg", "bG.svg", "bS.svg", "bN.svg", "bL.svg", "bP.svg",
-            "b+R.svg", "b+B.svg", "b+S.svg", "b+N.svg", "b+L.svg", "b+P.svg",
-            "wK.png", "wR.png", "wB.png", "wG.png", "wS.png", "wN.png", "wL.png", "wP.png",
-            "w+R.png", "w+B.png", "w+S.png", "w+N.png", "w+L.png", "w+P.png",
-            "bK.png", "bR.png", "bB.png", "bG.png", "bS.png", "bN.png", "bL.png", "bP.png",
-            "b+R.png", "b+B.png", "b+S.png", "b+N.png", "b+L.png", "b+P.png",
-        ),
-        "other": (
-            "wK.svg", "wR.svg", "wB.svg", "wG.svg", "wS.svg", "wN.svg", "wL.svg", "wP.svg",
-            "w+R.svg", "w+B.svg", "w+S.svg", "w+N.svg", "w+L.svg", "w+P.svg",
-            "bK.svg", "bR.svg", "bB.svg", "bG.svg", "bS.svg", "bN.svg", "bL.svg", "bP.svg",
-            "b+R.svg", "b+B.svg", "b+S.svg", "b+N.svg", "b+L.svg", "b+P.svg",
-        ),
-        "lishogi": (
-            "wK.svg", "wR.svg", "wB.svg", "wG.svg", "wS.svg", "wN.svg", "wL.svg", "wP.svg",
-            "w+R.svg", "w+B.svg", "w+S.svg", "w+N.svg", "w+L.svg", "w+P.svg",
-            "bK.svg", "bR.svg", "bB.svg", "bG.svg", "bS.svg", "bN.svg", "bL.svg", "bP.svg",
-            "b+R.svg", "b+B.svg", "b+S.svg", "b+N.svg", "b+L.svg", "b+P.svg",
-            "wK.png", "wR.png", "wB.png", "wG.png", "wS.png", "wN.png", "wL.png", "wP.png",
-            "w+R.png", "w+B.png", "w+S.png", "w+N.png", "w+L.png", "w+P.png",
-            "bK.png", "bR.png", "bB.png", "bG.png", "bS.png", "bN.png", "bL.png", "bP.png",
-            "b+R.png", "b+B.png", "b+S.png", "b+N.png", "b+L.png", "b+P.png",
-        ),
-        "shogi-themes": (
-            "wK.svg", "wR.svg", "wB.svg", "wG.svg", "wS.svg", "wN.svg", "wL.svg", "wP.svg",
-            "w+R.svg", "w+B.svg", "w+S.svg", "w+N.svg", "w+L.svg", "w+P.svg",
-            "bK.svg", "bR.svg", "bB.svg", "bG.svg", "bS.svg", "bN.svg", "bL.svg", "bP.svg",
-            "b+R.svg", "b+B.svg", "b+S.svg", "b+N.svg", "b+L.svg", "b+P.svg",
-        ),
-    },
-)
+    return GameRenderConfig(
+        piece_sets=tuple(piece_sets),
+        piece_file_names_by_provider={
+            k: tuple(sorted(v)) for k, v in file_names_by_provider.items()
+        },
+    )
 
 
 PLACEHOLDER_RENDER_CONFIG = GameRenderConfig(
@@ -284,9 +59,9 @@ PLACEHOLDER_RENDER_CONFIG = GameRenderConfig(
 
 
 GAME_RENDER_CONFIGS: dict[str, GameRenderConfig] = {
-    "chess": CHESS_RENDER_CONFIG,
-    "xiangqi": XIANGQI_RENDER_CONFIG,
-    "shogi": SHOGI_RENDER_CONFIG,
+    game_dir.name: _discover_render_config(game_dir.name)
+    for game_dir in sorted(PIECES_DIR.iterdir())
+    if game_dir.is_dir()
 }
 
 
